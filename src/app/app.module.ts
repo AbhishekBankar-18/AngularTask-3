fa519a1d4b54cbc97777aa572e39c4fc03dc04bd
AbhishekBankar-18 <abhishekbankar706@gmail.com>
2023-09-11 17:53:14 +0530

    task-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'primeng/accordion';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordianDataComponent } from './accordian-data/accordian-data.component';
+import { FormComponent } from './form/form.component';
+import { DataHandlerService } from './service/data-handler.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    AccordianDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AccordionModule,
+    ReactiveFormsModule
+    
   ],
-  providers: [],
+  providers: [
+    DataHandlerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
